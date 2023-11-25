fa4067243292c5fbc1c2c82572e352745c1888b1
biggaji <tobiajibade@yandex.com>
2023-11-25 13:43:03 +0100

    wrote set reminder control and bot interaction

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ import express from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import nodeCron from 'node-cron';
 dayjs.extend(localizedFormat);
 const app = express();
 const server = createServer(app);
 const ws = new Server(server, {
     cors: {
-        origin: 'http://localhost:5500',
+        origin: '*',
     },
 });
 app.use(cors({
@@ -52,6 +53,32 @@ app.post('/users/', (req, res, next) => {
         next(error);
     }
 });
+app.post('/reminders', async (req, res, next) => {
+    try {
+        const { description, dateTime } = req.body;
+        if (!description || !dateTime) {
+            throw new Error('All argument is required');
+        }
+        // Add logic to store in database
+        // Construct cron expression
+        const cronExpression = convertDateTimeToCronExpression(dateTime);
+        // // Schedule reminder
+        scheduleReminder({
+            cronExpression,
+            message: `Reminder set successfully`,
+        }, async () => {
+            ws.to(botSocketId).emit('REMINDER:ACTIVE');
+            // Logic to update database notified = yes
+        });
+        // Notify bot to update UI
+        ws.to(botSocketId).emit('REMINDER:SET', 'Reminder set');
+        // Return 200 for acknowledgemnt
+        res.status(200).json({});
+    }
+    catch (error) {
+        next(error);
+    }
+});
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log('Server running on port', PORT);
@@ -65,5 +92,46 @@ function capitalizeFirstLetter(word) {
     else {
         // Return an empty string if the input word is empty
         return '';
+    }
+}
+/**
+ * Converts a dateTime string from a date input to a cron expression.
+ * @param dateTime - The input dateTime string in a recognized format.
+ * @returns {string} - A cron expression representing the given dateTime.
+ * @throws {TypeError} If the argument 'dateTime' is not of type 'string.'
+ * @throws {Error} If an error occurs during the conversion, providing specific details.
+ */
+function convertDateTimeToCronExpression(dateTime) {
+    try {
+        if (typeof dateTime !== 'string') {
+            throw new TypeError("Expecting argument 'dateTime' to be a string");
+        }
+        const dateInstance = new Date(dateTime);
+        const minute = dateInstance.getMinutes();
+        const hour = dateInstance.getHours();
+        const date = dateInstance.getDate();
+        const month = dateInstance.getMonth() + 1; // Months are zero-based
+        const week = dateInstance.getDay();
+        // Construct and return the cron expression
+        return `${minute} ${hour} ${date} ${month} ${week}`;
+    }
+    catch (error) {
+        throw new Error(`Error converting date to cron expression: ${error.message}`);
+    }
+}
+function scheduleReminder(payload, callback) {
+    try {
+        const { cronExpression, message } = payload;
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        if (typeof callback !== 'function') {
+            throw new TypeError("Expecting argument'callback' to be a function");
+        }
+        // Add logic as you wish
+        nodeCron.schedule(cronExpression, callback, {
+            timezone: timeZone,
+        });
+    }
+    catch (error) {
+        throw new Error(`Error scheduling reminde: ${error.message}`);
     }
 }
--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import nodeCron from 'node-cron';
 
 dayjs.extend(localizedFormat);
 
@@ -12,7 +13,7 @@ const app = express();
 const server = createServer(app);
 const ws = new Server(server, {
   cors: {
-    origin: 'http://localhost:5500',
+    origin: '*',
   },
 });
 
@@ -66,6 +67,45 @@ app.post('/users/', (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
+app.post(
+  '/reminders',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { description, dateTime } = req.body;
+
+      if (!description || !dateTime) {
+        throw new Error('All argument is required');
+      }
+
+      // Add logic to store in database
+
+      // Construct cron expression
+      const cronExpression = convertDateTimeToCronExpression(dateTime);
+
+      // // Schedule reminder
+      scheduleReminder(
+        {
+          cronExpression,
+          message: `Reminder set successfully`,
+        },
+        async () => {
+          ws.to(botSocketId).emit('REMINDER:ACTIVE');
+
+          // Logic to update database notified = yes
+        },
+      );
+
+      // Notify bot to update UI
+      ws.to(botSocketId).emit('REMINDER:SET', 'Reminder set');
+
+      // Return 200 for acknowledgemnt
+      res.status(200).json({});
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log('Server running on port', PORT);
@@ -79,5 +119,61 @@ function capitalizeFirstLetter(word: string) {
   } else {
     // Return an empty string if the input word is empty
     return '';
+  }
+}
+
+/**
+ * Converts a dateTime string from a date input to a cron expression.
+ * @param dateTime - The input dateTime string in a recognized format.
+ * @returns {string} - A cron expression representing the given dateTime.
+ * @throws {TypeError} If the argument 'dateTime' is not of type 'string.'
+ * @throws {Error} If an error occurs during the conversion, providing specific details.
+ */
+function convertDateTimeToCronExpression(dateTime: string): string {
+  try {
+    if (typeof dateTime !== 'string') {
+      throw new TypeError("Expecting argument 'dateTime' to be a string");
+    }
+
+    const dateInstance = new Date(dateTime);
+    const minute = dateInstance.getMinutes();
+    const hour = dateInstance.getHours();
+    const date = dateInstance.getDate();
+    const month = dateInstance.getMonth() + 1; // Months are zero-based
+    const week = dateInstance.getDay();
+
+    // Construct and return the cron expression
+    return `${minute} ${hour} ${date} ${month} ${week}`;
+  } catch (error: any) {
+    throw new Error(
+      `Error converting date to cron expression: ${error.message}`,
+    );
+  }
+}
+
+type ScheduleReminderPayload = {
+  cronExpression: string;
+  message: string;
+};
+
+function scheduleReminder(
+  payload: ScheduleReminderPayload,
+  callback: any,
+): void {
+  try {
+    const { cronExpression, message } = payload;
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    if (typeof callback !== 'function') {
+      throw new TypeError("Expecting argument'callback' to be a function");
+    }
+
+    // Add logic as you wish
+
+    nodeCron.schedule(cronExpression, callback, {
+      timezone: timeZone,
+    });
+  } catch (error: any) {
+    throw new Error(`Error scheduling reminde: ${error.message}`);
   }
 }
